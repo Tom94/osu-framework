7ab65d9d75b9c528b34de90a21a8052b08f64406
Thomas Müller <tom@94.me>
2026-09-11 19:25:47 +0900

    Update osu.Framework.Tests/Visual/Performance/TestSceneFillPerformance.cs
    Co-authored-by: Dean Herbert <pe@ppy.sh>

--- osu.Framework.Tests/Visual/Performance/TestSceneFillPerformance.cs
+++ osu.Framework.Tests/Visual/Performance/TestSceneFillPerformance.cs
@@ -54,7 +54,7 @@ namespace osu.Framework.Tests.Visual.Performance
 
             AddLabel("Drawables");
 
-            AddSliderStep("count", 1, 100, 100, v => DrawableCount.Value = v);
+            AddSliderStep("count", 1, 100, 1000, v => DrawableCount.Value = v);
             AddToggleStep("gradient colour", v => GradientColour.Value = v);
             AddToggleStep("randomise colour", v => RandomiseColour.Value = v);
 
